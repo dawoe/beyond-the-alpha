76887808afed370a75769e3680d9a7a39dcb3786
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2026-09-20 14:21:45 +0200

    Added search packages

---
+++ BeyondTheAlpha/DependencyInjection/SiteComposer.cs
@@ -0,0 +1,20 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Search.BackOffice.DependencyInjection;
+using Umbraco.Cms.Search.Core.DependencyInjection;
+using Umbraco.Cms.Search.DeliveryApi.DependencyInjection;
+using Umbraco.Cms.Search.Provider.Examine.DependencyInjection;
+
+namespace BeyondTheAlpha.DependencyInjection
+{
+	internal sealed class SiteComposer : IComposer
+	{
+		public void Compose(IUmbracoBuilder builder)
+		{
+			builder
+				.AddSearchCore()
+				.AddBackOfficeSearch()
+				.AddDeliveryApiSearch()
+				.AddExamineSearchProvider();
+		}
+	}
+}
